6bdd885fc4878e6bf06346375fb0de0754050222
leonardoroth <leonardoroth1039@gmail.com>
2024-07-12 22:48:53 -0300

    projeto proposto

---
+++ ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro dever ser maior que o primeiro");
+    }
+}
